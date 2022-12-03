767e28e1e89c8b4829f9ab06e4f09c88d4da7b08
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2022-12-04 00:29:20 +0900

    update

---
+++ schedule.py
@@ -0,0 +1,20 @@
+import subprocess
+
+model_list = ['LSTM', 'BaseTransformer']
+look_back_list = range(5, 41, 5)
+params_list = []
+for model in model_list:
+    for look_back in look_back_list:
+        params_list.append([model, look_back])
+
+for params in params_list:
+    command = [
+        'python', 'train.py',
+        '--model', str(params[0]),
+        '--look_back', str(params[1]),
+        '--e_name', 'Mazda Refrigerant Circuit Turtrial 20221207_2',
+        '--seed', '2',
+        '--epoch', '100'
+    ]
+    print('command: ', command)
+    subprocess.run(command)
