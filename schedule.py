b39eef18922112979b39a94495bf9429c8875702
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2022-12-08 23:58:04 +0900

    update

--- schedule.py
+++ schedule.py
@@ -12,9 +12,8 @@ for params in params_list:
         'python', 'train.py',
         '--model', str(params[0]),
         '--look_back', str(params[1]),
-        '--e_name', 'Mazda Refrigerant Circuit Turtrial 20221207_2',
-        '--seed', '2',
-        '--epoch', '100'
+        '--e_name', 'Mazda Refrigerant Circuit Turtrial 20221207_3',
+        '--seed', '42',
     ]
-    print('command: ', command)
+    print('\ncommand: ', command)
     subprocess.run(command)
