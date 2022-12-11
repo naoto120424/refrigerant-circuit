60b371d13f66c135409a42de74d6e3aae5a4b796
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2022-12-12 01:46:37 +0900

    update utils

--- utils/utiles.py
+++ utils/utiles.py
@@ -33,8 +33,11 @@ model_list = {
     "BaseTransformer",
     "BaseTransformer_only1pe",
     "BaseTransformer_input_sensor_first",
+    "BaseTransformer_input_sensor_first_only1pe",
     "BaseTransformer_input_3types",
+    "BaseTransformer_input_3types_only1pe",
     "BaseTransformer_input_flattened",
+    "BaseTransformer_input_flattened_only1pe",
 }
 
 criterion_list = {"MSE": nn.MSELoss(), "L1": nn.L1Loss()}
@@ -60,12 +63,18 @@ def modelDecision(model, look_back, dim, depth, heads, fc_dim, dim_head, dropout
             from model.transformer.base_transformer import BaseTransformer
         elif model == "BaseTransformer_only1pe":
             from model.transformer.base_transformer_only1pe import BaseTransformer
-        elif model == "BaseTransformer_agent_first":
+        elif model == "BaseTransformer_input_sensor_first":
             from model.transformer.base_transformer_input_sensor_first import BaseTransformer
+        elif model == "BaseTransformer_input_sensor_first_only1pe":
+            from model.transformer.base_transformer_input_sensor_first_only1pe import BaseTransformer
         elif model == "BaseTransformer_input_3types":
             from model.transformer.base_transformer_input_3types import BaseTransformer
+        elif model == "BaseTransformer_input_3types_only1pe":
+            from model.transformer.base_transformer_input_3types_only1pe import BaseTransformer
         elif model == "BaseTransformer_input_flattened":
             from model.transformer.base_transformer_input_flattened import BaseTransformer
+        elif model == "BaseTransformer_input_flattened_only1pe":
+            from model.transformer.base_transformer_input_flattened_only1pe import BaseTransformer
 
         return BaseTransformer(look_back=look_back, dim=dim, depth=depth, heads=heads, fc_dim=fc_dim, dim_head=dim_head, dropout=dropout, emb_dropout=emb_dropout)
 
