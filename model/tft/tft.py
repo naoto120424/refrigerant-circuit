fef7eb5af0c0334d8cdb9a3f93a8560f344acbcc
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2023-01-07 18:57:27 +0900

    update

--- model/tft/tft.py
+++ model/tft/tft.py
@@ -111,3 +111,79 @@ class GatedResidualNetwork(nn.Module):
             output = self.glu_add_norm(n1, x)
 
         return output
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0, scale=True):
+        super(ScaledDotProductAttention, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.softmax = nn.Softmax(dim=2)
+        self.scale = scale
+
+    def forward(self, q, k, v, mask=None):
+        attn = torch.bmm(q, k.permute(0, 2, 1))
+
+        if self.scale:
+            dimention = torch.sqrt(torch.tensor(k.shape[-1]).to(torch.float32))
+            attn = attn / dimention
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+
+        output = torch.bmm(attn, v)
+
+        return output, attn
+
+
+class InterpretabMultiHeadAttention(nn.Module):
+    def __init__(self, n_head, d_model, dropout):
+        super(InterpretabMultiHeadAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_k = self.d_q = self.d_v = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.v_layer = nn.Linear(self.d_model, self.d_v, bias=False)
+        self.q_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_q, bias=False) for _ in range(self.n_head)])
+        self.k_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_k, bias=False) for _ in range(self.n_head)])
+        self.v_layers = nn.ModuleList([self.v_layer for _ in range(self.n_head)])
+        self.attention = ScaledDotProductAttention()
+        self.w_h = nn.Linear(self.d_v, self.d_model, bias=False)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if "bias" not in name:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.zeros_(p)
+
+    def forward(self, q, k, v, mask=None):
+        heads = []
+        attns = []
+        for i in range(self.n_head):
+            qs = self.q_layers[i](q)
+            ks = self.k_layers[i](k)
+            vs = self.v_layers[i](v)
+            head, attn = self.attention(qs, ks, vs, mask)
+            head_dropout = self.dropout(head)
+            heads.append(head_dropout)
+            attns.append(attn)
+
+        head = torch.stack(heads, dim=2) if self.n_head > 1 else heads[0]
+        attn = torch.stack(attns, dim=2)
+        outputs = torch.mean(head, dim=2) if self.n_head > 1 else head
+        outputs = self.w_h(outputs)
+        outputs = self.dropout(outputs)
+
+        return outputs, attn
+
+
+class VariableSelectionNetwork(nn.Module):
+    def __init__(self, hidden_layer_size, dropout_rate) -> None:
+        super().__init__()
