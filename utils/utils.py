7d8a0802b5a7ade3fd4909dd03cabb5c322c626d
naoto120424 <naoto-naka@hiroshima-u.ac.jp>
2023-08-01 01:09:00 +0900

    update

--- utils/utils.py
+++ utils/utils.py
@@ -40,7 +40,6 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.set_float32_matmul_precision("high")
 
 
 # model decide from model name
